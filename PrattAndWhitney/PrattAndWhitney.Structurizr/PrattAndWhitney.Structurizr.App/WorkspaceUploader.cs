87d9bdae77b837892c29d3a0ba4f89f2a5112bcd
BK <bhavesh.khatri@gmail.com>
2018-03-19 16:30:23 -0500

    Refactoring.

---
+++ PrattAndWhitney/PrattAndWhitney.Structurizr/PrattAndWhitney.Structurizr.App/WorkspaceUploader.cs
@@ -0,0 +1,19 @@
+﻿using Structurizr;
+using Structurizr.Api;
+using System.Configuration;
+
+namespace PrattAndWhitney.Structurizr.App
+{
+    public static class WorkspaceUploader
+    {
+        public static void Upload(Workspace workspace)
+        {
+            var WorkspaceId = long.Parse(ConfigurationManager.AppSettings["WorkspaceId"]);
+            var ApiKey = ConfigurationManager.AppSettings["ApiKey"];
+            var ApiSecret = ConfigurationManager.AppSettings["ApiSecret"];
+
+            StructurizrClient structurizrClient = new StructurizrClient(ApiKey, ApiSecret);
+            structurizrClient.PutWorkspace(WorkspaceId, workspace);
+        }
+    }
+}
