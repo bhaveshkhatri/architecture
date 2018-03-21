68da6838d76dbe8a8d4dc5d05f7509a2054e1542
BK <bhavesh.khatri@gmail.com>
2018-03-21 08:45:07 -0500

    Refactoring.

--- PrattAndWhitney/PrattAndWhitney.Structurizr/PrattAndWhitney.Structurizr.App/WorkspaceConfiguration/Deployments.cs
+++ PrattAndWhitney/PrattAndWhitney.Structurizr/PrattAndWhitney.Structurizr.App/WorkspaceConfiguration/Deployments.cs
@@ -6,23 +6,25 @@ namespace PrattAndWhitney.Structurizr.App.WorkspaceConfiguration
     {
         public static void Configure(Model model)
         {
-            var clientDesktop = model.AddDeploymentNode("ITS Client Desktop", "The desktop environment of an ITS user", "TBD - Windows 7");
+            model.AddDeploymentNode("ITS Client Desktop", "The desktop environment of an ITS user", "TBD - Windows 7")
+                .AddDeploymentNode("Desktop Browser", "Client Desktop Browser", "TBD - Internet Explorer")
+                .AddDeploymentNode("ITS SPA", "ITS Single Page Application", "TBD - Angular")
+                .Add(Containers.TargetSystem.WebClient);
 
-            var desktopBrowser = clientDesktop.AddDeploymentNode("Desktop Browser", "Client Desktop Browser", "TBD - Internet Explorer");
-            desktopBrowser.AddDeploymentNode("ITS SPA", "ITS Single Page Application", "TBD - Angular").Add(Containers.TargetSystem.WebClient);
-
-            var clientMobileDevice = model.AddDeploymentNode("ITS Client Mobile", "The mobile environment of an ITS user", "TBD - iPhone 6+ iOS 10+");
-
-            var mobileBrowser = clientMobileDevice.AddDeploymentNode("Mobile Browser", "Client Mobile Browser", "TBD - Safari");
-            mobileBrowser.AddDeploymentNode("ITS SPA", "ITS Single Page Application", "TBD - Angular").Add(Containers.TargetSystem.WebClient);
+            model.AddDeploymentNode("ITS Client Mobile", "The mobile environment of an ITS user", "TBD - iPhone 6+ iOS 10+")
+                .AddDeploymentNode("Mobile Browser", "Client Mobile Browser", "TBD - Safari")
+                .AddDeploymentNode("ITS SPA", "ITS Single Page Application", "TBD - Angular")
+                .Add(Containers.TargetSystem.WebClient);
 
             var datacenter = model.AddDeploymentNode("Pratt & Whitney Datacenter", "Pratt & Whitney Datacenter", "TBD - Azure");
 
-            var webApplicationServer = datacenter.AddDeploymentNode("WebServer***", "A web server VM residing in Azure.", "Windows Server 2016", 2);
-            webApplicationServer.AddDeploymentNode("IIS", "A web server from Microsoft.", "IIS 10.0", 1).Add(Containers.TargetSystem.WebApplication);
+            datacenter.AddDeploymentNode("WebServer***", "A web server VM residing in Azure.", "Windows Server 2016", 2)
+                .AddDeploymentNode("IIS", "A web server from Microsoft.", "IIS 10.0", 1)
+                .Add(Containers.TargetSystem.WebApplication);
 
-            var apiServer = datacenter.AddDeploymentNode("APIServer***", "An API server VM residing in Azure.", "Windows Server 2016", 2);
-            apiServer.AddDeploymentNode("IIS", "A web server from Microsoft.", "IIS 10.0", 1).Add(Containers.TargetSystem.ApiGatewayService);
+            datacenter.AddDeploymentNode("APIServer***", "An API server VM residing in Azure.", "Windows Server 2016", 2)
+                .AddDeploymentNode("IIS", "A web server from Microsoft.", "IIS 10.0", 1)
+                .Add(Containers.TargetSystem.ApiGatewayService);
         }
     }
 }
